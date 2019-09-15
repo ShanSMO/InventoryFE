0a961f835e391684012b1240d70e1e4db343b287
shanaka <madushanka991@gmail.com>
2019-09-15 21:46:15 +0530

    changes

--- src/app/global-properties.ts
+++ src/app/global-properties.ts
@@ -1,7 +1,7 @@
 export class GlobalProperties  {
 
-  public static API_URL = 'http://192.168.8.102:8080/inventory/';
-  public static RESOURCE_URL = 'http://192.168.8.102:8080/';
+  public static API_URL = 'http://127.0.0.1:8080/inventory/';
+  public static RESOURCE_URL = 'http://127.0.0.1:8080/';
   public static APP_NAME = 'inventory/';
 
   // public static API_URL = 'http://ec2-52-15-205-184.us-east-2.compute.amazonaws.com:8080/inventory/';
