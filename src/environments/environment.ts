ef2381d33782030f84186656fad96320d82e78be
Shanaka <madushanka991@gmail.com>
2020-05-16 19:12:48 +0530

    prop changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment : {
   production: false,
-  API_URL : 'http://127.0.0.1:8080/inventory/',
-  RESOURCE_URL : 'http://127.0.0.1:8080/',
-  APP_NAME : 'inventory/'
+  API_URL: 'http://127.0.0.1:8080/inventory/',
+  RESOURCE_URL: 'http://127.0.0.1:8080/',
+  APP_NAME: 'inventory/'
 };
